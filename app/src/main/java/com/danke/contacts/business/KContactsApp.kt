66afc9e178611fafe4ffe31f7f98c07a96bccb27
danke77 <xue_xiaozhe@163.com>
2016-07-18 13:52:32 +0800

    leak canary

--- app/src/main/java/com/danke/contacts/business/KContactsApp.kt
+++ app/src/main/java/com/danke/contacts/business/KContactsApp.kt
@@ -3,6 +3,7 @@ package com.danke.contacts.business
 import android.app.Application
 import com.danke.contacts.business.user.entity.UserInfoEntity
 import com.danke.contacts.medium.extensions.DelegatesExt
+import com.squareup.leakcanary.LeakCanary
 import java.util.*
 
 /**
@@ -20,6 +21,9 @@ class KContactsApp : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        // LeakCanary detects memory leaks
+        LeakCanary.install(this)
     }
 
 }
